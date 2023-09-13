6f3d26902d3262e6ff5b1c7042c07d4a0773da89
stevemkd <stephintomy2000@gmail.com>
2023-09-13 13:49:43 +0530

    Add files via upload

---
+++ Simple website/login/script.js
@@ -0,0 +1,56 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const Email = () => {
+    let EMAIL = email.value.trim();
+  
+    if (EMAIL === "") {
+      setError(email, "Email is required");
+    } else if (!isValidEmail(EMAIL)) {
+      setError(email, "Provide a valid email address");
+    } else {
+      setSuccess(email);
+    }
+  };
+  const Password = () => {
+    let PASSWORD = password.value.trim();
+  
+    if (PASSWORD === "") {
+      setError(password, "Password is required");
+    } else if (PASSWORD.length < 6) {
+      setError(password, "Password must be at least 6 character.");
+    } else {
+      setSuccess(password);
+    }
+  };
